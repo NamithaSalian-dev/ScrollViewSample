7aa976674f838f85c91a31701f68d2a3c4ae297c
NamithaSalianDev <namitha.j@dxc.com>
2021-10-15 15:35:16 +0530

    Initial Commit

---
+++ ScrollViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollViewSample
+//
+//  Created by namitha on 15/10/21.
+//  Copyright © 2021 Namitha J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
